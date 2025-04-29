206d5dffc1e1458de672b8283cdd7243867103be
AryanS008 <singh8aryan.12@gmail.com>
2025-04-29 16:50:04 +0530

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,12 +12,18 @@ import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
 import AddAddress from './pages/AddAddress';
 import MyOrders from './pages/MyOrders';
+import SellerLogin from './components/seller/SellerLogin';
+import SellerLayout from './pages/seller/SellerLayout';
+import AddProduct from './components/seller/AddProduct';
+import ProductList from './components/seller/ProductList';
+import Orders from './components/seller/Orders';
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
-  const {showUserLogin} = useAppContext();
+  const {showUserLogin, isSeller} = useAppContext();
   return (
-    <div > 
+    <div className='tex-default min-h-screen text-gray-700 bg-white'> 
+
       {isSellerPath ? null :<Navbar />}
       {showUserLogin ?<Login/> : null}
 
@@ -32,6 +38,12 @@ const App = () => {
           <Route path="/cart" element={<Cart/>} />
           <Route path="/add-address" element={<AddAddress/>} />
           <Route path="/my-orders" element={<MyOrders/>} />
+          <Route path="/seller" element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
+          <Route index element={isSeller ? <AddProduct/> : null} />
+          <Route path='product-list' element={<ProductList/>} />
+          <Route path='orders' element={<Orders/>} />         
+
+          </Route>
         </Routes>
       </div>
      {!isSellerPath && <Footer/>}
